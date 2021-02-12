1098f8623a5551c3650b90742996ee3603a0d5f2
LeandroJOliveira <43957257+LeandroJOliveira@users.noreply.github.com>
2021-02-12 13:00:21 -0300

    Projeto1Acesso

--- spec/acesso_spec.rb
+++ spec/acesso_spec.rb
@@ -2,7 +2,7 @@ describe "Login Acesso" do
 
     it "Abrindo a página - Acesso" do
         visit "http://laboratorio.mdacesso.com.br/logon.aspx"
-        sleep 5 #temporario
+        sleep 8 #temporario
         expect(page.title).to eql "Sistema de Controle de Acesso"
     end
 end
